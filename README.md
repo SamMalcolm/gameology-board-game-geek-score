d73df5518a9acb989d1b4f283c1f1fff52296250
Sam Malcolm <samuel.malcolm@rmit.edu.au>
2019-02-11 00:11:33 +1100

    added readme and changes to bggScore.js

---
+++ README.md
@@ -0,0 +1,5 @@
+# Add Board Game Geek Score to Gameology.com.au product pages
+
+A simple browser extension to add the BGG score to product pages on the gameology site, so that you dont need to be between the sites to get the score.
+Not currently functioning as expected as I only just started it.
+Uses the board game geek XML API
--- bggScore.js
+++ bggScore.js
@@ -1,19 +1,65 @@
-document.addEventListener("DOMContentLoaded", function (e) {
-    let xhr = new XMLHttpRequest();
-    let parser = new DOMParser();
-    xhr.open("GET","https://api.geekdo.com/xmlapi2/search?query="+encodeURI(document.querySelector(".product-name").innerHTML));
-    xhr.onreadystatechange = function() {
-        console.log("XHR STATUS: "+xhr.status);
-        console.log("XHR READY STATE: "+xhr.readyState);
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            try {
-                let xmlSearchDoc = parser(xhr.responseText,"text/xml");
-
-            }
-            catch(err) {
-                console.log(err);
+function promiseGet(path) {
+    return new Promise(function (resolve, reject) {
+        let xhr = new XMLHttpRequest();
+        xhr.open("GET",path);
+        xhr.onreadystatechange = function() {
+            console.log("XHR STATUS: "+xhr.status);
+            console.log("XHR READY STATE: "+xhr.readyState);
+            if (xhr.readyState === 4) {
+                if (xhr.status === 200) {
+                    resolve(xhr.responseText);
+                } else {
+                    resolve(xhr.status+": "+xhr.statusText);
+                }
             }
         }
+        xhr.send();
+    });
+}
 
+function injectWidget() {
+    let widgetMarkup = `
+        <div class="bgg_widget">
+            <div class="bgg_widget_loader"></div>
+            <div class="bgg_widget_name_container">
+                Name: <span class="bgg_widget_name"></span>
+            </div> 
+            <div class="bgg_widget_score_container">
+                Score: <span class="bgg_widget_score"></span>
+            </div>      
+        </div>
+    `;
+    document.querySelector("body").innerHTML += widgetMarkup;
+}
+
+function setLoader(add) {
+    if (add) {
+        document.querySelector(".bgg_widget_loader").style.display = "block";
+    } else {
+        document.querySelector(".bgg_widget_loader").style.display = "none";
     }
-}, false);
+}
+
+function updateWidget(score, name) {
+    document.querySelector(".bgg_widget_score").innerHTML = score;
+    document.querySelector(".bgg_widget_name").innerHTML = name;
+}
+
+injectWidget();
+setLoader(true);
+let parser = new DOMParser();
+let xmlSearchDoc = await promiseGet("https://api.geekdo.com/xmlapi2/search?query="+encodeURI(document.querySelector(".product-name").innerHTML));
+let xmlSearchDoc = parser.parseFromString(xmlSearchDoc, 'text/xml');
+if (xmlSearchDoc.getElementsByTagName("item")[0]) {
+    let id = xmlSearchDoc.getElementsByTagName("item")[0].getAttribute("id");
+    console.log("ID: "+id);
+    let xmlScoreDoc = await promiseGet("https://api.geekdo.com/xmlapi2/thing?id="+id+"&stats=1");
+    xmlScoreDoc = parser.parseFromString(xmlScoreDoc, 'text/xml');
+    let name = xmlScoreDoc.getElementsByTagName("name")[0].getAttribute("value");
+    let score = Math.round(parseInt(xmlScoreDoc.getElementsByTagName("average")[0].getAttribute("value")) * 10 ) / 10;
+    updateWidget(score,name);
+    setLoader(false);
+} else {
+    console.log("Item not found on board game geek");
+}
+    
